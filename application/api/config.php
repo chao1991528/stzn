43279b002067b15792e20a24b122b7d934cb521e
zhouyuchao <392318709@qq.com>
2019-01-22 10:20:18 +0800

    通用版本1.0

--- application/api/config.php
+++ application/api/config.php
@@ -3,4 +3,5 @@
 //配置文件
 return [
     'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
+    'api_secret_key'          => 'FTQAZ1Aa'
 ];
--- application/config.php
+++ application/config.php
@@ -242,6 +242,9 @@ return [
         // 验证成功后是否重置
         'reset'    => true
     ],
+    'image_domain'              => Env::get('app.image_domain', ''), //图片域名
+    'domain'                    => Env::get('app.domain', ''), //项目域名
+    'skip_api_sign_check'       => Env::get('app.skip_api_sign_check', false),
     // +----------------------------------------------------------------------
     // | Token设置
     // +----------------------------------------------------------------------
